3c3b64a81d76856c42219c381d6d22ce1a71d575
성민 <163387941+lovehachiware@users.noreply.github.com>
2025-04-09 02:29:03 +0900

    Title: 최소공배수 구하기, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250409/최소공배수 구하기/find-the-least-common-multiple.py
+++ 250409/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,15 +1,20 @@
-# 변수 선언 및 입력:
-n, m = tuple(map(int, input().split()))
-
-
-# n과 m의 최대공약수를 반환합니다.
+# 최대공약수 함수 (GCD)
 def find_gcd(n, m):
     gcd = 0
-    for i in range(max(n, m), 0,-1):
+    for i in range(1, min(n, m) + 1):
         if n % i == 0 and m % i == 0:
             gcd = i
+    return gcd
 
-    print(gcd)
+# 최소공배수 함수 (LCM)
+def find_lcm(n, m):
+    gcd = find_gcd(n, m)  # 먼저 최대공약수를 구하고
+    lcm = (n * m) // gcd  # 공식을 사용해서 최소공배수를 구함
+    return lcm
 
+# 입력 받기
+n, m = map(int, input().split())
 
-find_gcd(n, m)
+# 결과 출력
+
+print(find_lcm(n, m))
